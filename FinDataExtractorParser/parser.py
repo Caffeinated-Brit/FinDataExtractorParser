abd9a0e4326298394fad5fd60751234e7c307f7a
Alrondes <almondisland46@gmail.com>
2024-11-14 00:42:05 -0500

    Fix CORS access to allow all ips on http://10.222.199.90:5000

--- FinDataExtractorParser/parser.py
+++ FinDataExtractorParser/parser.py
@@ -60,4 +60,4 @@ def get_json():
     return jsonify({"status": "success", "message": "GET request received"}), 200
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
