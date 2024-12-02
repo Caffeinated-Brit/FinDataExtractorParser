e64f4309a1ea785445485406baeda6f49ac5d9f1
SLS-BSU <spencer.smith2@bsu.edu>
2024-12-02 13:45:02 -0500

    Remove accidental comment line.

--- download_llm.py
+++ download_llm.py
@@ -11,7 +11,7 @@ BASE_DIR = "FinDataExtractorParser/LLMs"
 # MODEL_URL = "https://huggingface.co/bartowski/Meta-Llama-3.1-8B-Instruct-GGUF/resolve/main/Meta-Llama-3.1-8B-Instruct-Q8_0.gguf"
 
 #MODEL_URL = "https://huggingface.co/bartowski/Meta-Llama-3.1-8B-Instruct-GGUF/resolve/main/Meta-Llama-3.1-8B-Instruct-Q5_K_L.gguf"
-# first test of mistral. seems to be useless
+
 MODEL_URL = "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q6_K.gguf"
 
 MODEL_FILE = "mistral-7b-instruct-v0.2.Q6_K.gguf"
